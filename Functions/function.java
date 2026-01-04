8493e27fde54c978a60738875fcd11db6e798bc0
Jasim-10 <jasimcec2025@gmail.com>
2026-01-04 21:36:20 +0530

    Java

--- Functions/function.java
+++ Functions/function.java
@@ -219,3 +219,33 @@
 
 
 
+//binary to decimal
+// import java.util.*;
+// public class function{
+//     public static void binToDec(int binNum){
+//         int myNum = binNum;
+//         int pow = 0;
+//         int decNum = 0;
+
+//         while(binNum>0){
+//             int lastDigit = binNum % 10;
+//             decNum = decNum + (lastDigit * (int)Math.pow(2,pow));
+
+//             pow++;
+//             binNum = binNum/10;
+//         }
+//         System.out.println("decimal of "+ myNum + " = " +decNum);
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the binary number: ");
+//         int num = sc.nextInt();
+//         binToDec(num);
+//     }
+// }
+
+
+
+
+
