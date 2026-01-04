24de2f0a54c7485e1db504213bc903cb649caa4d
Jasim-10 <jasimcec2025@gmail.com>
2026-01-04 21:46:42 +0530

    Java

--- Functions/function.java
+++ Functions/function.java
@@ -249,3 +249,27 @@
 
 
 
+// import java.util.*;
+// public class function{
+//     public static void decToBin(int n){
+//         int myNum = n;
+//         int pow = 0;
+//         int binNum = 0;
+
+//         while(n>0){
+//             int rem = n%2;
+//             binNum = binNum + (rem * (int)Math.pow(10,pow));
+
+//             pow++;
+//             n/=2;
+//         }
+//         System.out.println("binary number of "+myNum + "= "+binNum);
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the binary number: ");
+//         int num = sc.nextInt();
+//         decToBin(num);
+//     }
+// }
